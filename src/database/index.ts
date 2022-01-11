4fb5349b8277b7d92d06e4eae2f632fe294eeb53
Felipe <fellipefreiire3@gmail.com>
2022-01-11 16:49:49 -0300

    changes

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,12 @@
-import { createConnection } from 'typeorm';
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-(async () => await createConnection())();
+export default async (host = 'database_fin_api'): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host,
+      database: process.env.NODE_ENV === 'test' ? 'fin_api_test' : defaultOptions.database,
+    }),
+  );
+};
