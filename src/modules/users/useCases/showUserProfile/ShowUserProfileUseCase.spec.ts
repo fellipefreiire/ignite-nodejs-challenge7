1a3acc3dd385758e7feb87c794d7b4383e667111
Felipe <fellipefreiire3@gmail.com>
2022-01-11 21:16:47 -0300

    show user profile test with error

---
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.spec.ts
@@ -0,0 +1,40 @@
+import { InMemoryUsersRepository } from '@modules/users/repositories/in-memory/InMemoryUsersRepository';
+import { AppError } from '@shared/errors/AppError';
+import { AuthenticateUserUseCase } from './../authenticateUser/AuthenticateUserUseCase';
+import { CreateUserUseCase } from './../createUser/CreateUserUseCase';
+import { ShowUserProfileUseCase } from './ShowUserProfileUseCase';
+
+
+let createUserUseCase: CreateUserUseCase
+let authenticateUserUseCase: AuthenticateUserUseCase
+let inMemoryUsersRepository: InMemoryUsersRepository
+let showUserProfileUseCase: ShowUserProfileUseCase
+
+describe('Show user profile', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    authenticateUserUseCase = new AuthenticateUserUseCase(inMemoryUsersRepository)
+    showUserProfileUseCase = new ShowUserProfileUseCase(inMemoryUsersRepository)
+    createUserUseCase = new CreateUserUseCase(inMemoryUsersRepository)
+  })
+
+  it('should be able to show user profile', async () => {
+    const user = await createUserUseCase.execute({
+      name: 'Test user',
+      email: 'test@email.com',
+      password: 'Test password',
+    })
+
+    const userProfile = await showUserProfileUseCase.execute(user.id)
+
+    expect(userProfile).toEqual(user)
+  })
+
+  //TODO Show Profile Error
+  // it('should not be able to show invalid user profile', async () => {
+  //   const userProfile = await showUserProfileUseCase.execute("Test user id")
+  //   console.log(userProfile)
+
+  //   expect(userProfile).toEqual(null)
+  // })
+})
